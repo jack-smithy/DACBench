2f2d0a2afd1ee433a0a84fed06920495fc1ad888
Jack Smith <js505@st-andrews.ac.uk>
2023-07-26 13:33:25 +0100

    better

--- dacbench/envs/modcma_pop_size.py
+++ dacbench/envs/modcma_pop_size.py
@@ -37,6 +37,9 @@ class CMAESPopSizeEnv(AbstractEnv):
 
         self.get_reward = self.get_default_reward
         self.get_state = self.get_default_state
+        
+        self.previous_obj_best = -1 * np.inf
+        self.current_obj_best = -1 * np.inf
 
     def step(self, action):
         """
@@ -53,7 +56,7 @@ class CMAESPopSizeEnv(AbstractEnv):
             state, reward, done, info 
         """
         
-        tol = 1e-8
+        self.previous_obj_best = self.current_obj_best
         
         truncated = super(CMAESPopSizeEnv, self).step_()
         terminated = not self.es.step()
@@ -61,14 +64,9 @@ class CMAESPopSizeEnv(AbstractEnv):
         if not (terminated or truncated):
             """Moves forward in time one step"""
             self.es.parameters.update_popsize(round(min(max(action[0], 4), 512*10)))
-            
-            print(self.get_state(self))
-            print(self.get_reward(self))
-            print('')
-            
-            if abs(self.get_reward(self)) < tol:
-                truncated = True
-            
+
+        self.current_obj_best = self.objective.state.current_best.y
+    
         return self.get_state(self), self.get_reward(self), terminated, truncated, {}
 
     def reset(self, seed=None, options={}):
@@ -82,8 +80,10 @@ class CMAESPopSizeEnv(AbstractEnv):
         """
         super(CMAESPopSizeEnv, self).reset_(seed)
         
-        self.dim = self.instance[0]
-        self.fid = self.instance[1]
+        self.dim = self.instance[1]
+        self.fid = self.instance[0]
+        
+        print(f'FID{self.fid}, dim={self.dim}')
         
         self.objective = ioh.get_problem(
             fid = self.fid,
@@ -137,26 +137,17 @@ class CMAESPopSizeEnv(AbstractEnv):
             Reward
 
         """
-        opt = self.objective.optimum
-        dy =  self.objective.state.current_best.y - opt.y
-        
-        reward = min(self.reward_range[1], max(self.reward_range[0], -1 * dy))
-        return reward
+
+        return self.previous_obj_best - self.current_obj_best
 
     def get_default_state(self, _):
         """
         Gather state description
-
-        Returns
-        -------
-        dict
-            Environment state
-
         """
-
-        state = {
-            "lambda_": self.es.parameters.lambda_,
-            "ptnorm": self.es.parameters.pnorm,
-            "normalisation_factor": self.es.parameters.expected_update_snorm(),
-        }
+        
+        state = [
+            self.es.parameters.lambda_,
+            self.es.parameters.pnorm,
+            self.es.parameters.expected_update_snorm(),
+        ]
         return state
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.td3 import TD3
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.monitor import Monitor
 
 from dacbench.wrappers import PerformanceTrackingWrapper, RewardNoiseWrapper
 from dacbench.logger import Logger, load_logs, log2dataframe
@@ -15,14 +16,18 @@ from dacbench.benchmarks import CMAESPopSizeBenchmark
 
 bench = CMAESPopSizeBenchmark()
 env = bench.get_environment()
-print(env.instance_set)
+env = Monitor(env, "./logs/")
 
 n_actions = env.action_space.shape[-1]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
-agent = TD3("MultiInputPolicy", env, action_noise=action_noise, verbose=1)
-agent.learn(total_timesteps=10000, log_interval=50)
-agent.save('test1')
+eval_callback = EvalCallback(env, best_model_save_path="./logs/",
+                             log_path="./logs/", eval_freq=500,
+                             deterministic=True, render=False)
+
+agent = TD3("MlpPolicy", env, action_noise=action_noise, verbose=1)
+agent.learn(total_timesteps=1e5, callback=eval_callback, progress_bar=True)
+agent.save('./logs/test1')
 
 vec_env = agent.get_env()
 
@@ -30,8 +35,9 @@ del agent
 
 model = TD3.load("test1")
 
+print(vec_env)
+
 obs = vec_env.reset()
 while True:
-    print(obs)
-    action, _states = model.predict(obs)
+    action, _states = model.predict(obs[0])
     obs, rewards, dones, info = env.step(action)
